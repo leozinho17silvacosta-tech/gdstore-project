e5c9c12474435c5b57ca82cbe8083669e3a780cd
Leo <leozinho17silvacosta@gmail.com>
2026-06-02 20:18:45 -0700

    Fixed errors

--- frontend/GdStore/vite.config.ts
+++ frontend/GdStore/vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: "src",
   plugins: [react(), tailwindcss()],
-  resolve: {
-    dedupe: ["react", "react-dom"],
-  },
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-  },
 });
